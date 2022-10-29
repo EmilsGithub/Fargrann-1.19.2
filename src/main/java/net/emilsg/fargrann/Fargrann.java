44742e293cc5789ff3f49281ffdd8d7daca1b97f
Emil <emilsvardgronvikacc@gmail.com>
2022-10-29 03:13:35 +0200

    Initial commit

---
+++ src/main/java/net/emilsg/fargrann/Fargrann.java
@@ -0,0 +1,15 @@
+package net.emilsg.fargrann;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Fargrann implements ModInitializer {
+	public static final String MOD_ID = "fargrann";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/emilsg/fargrann/FargrannClient.java
@@ -0,0 +1,12 @@
+package net.emilsg.fargrann;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FargrannClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
